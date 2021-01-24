207c1f34dc007b5323fbc15c2577ba4e3f06f182
BluBloos <cnoah1705@gmail.com>
2021-01-24 00:13:17 -0500

    Some small changes

--- frontend/qhacks/src/pages/index.js
+++ frontend/qhacks/src/pages/index.js
@@ -1,24 +1,38 @@
 import React from "react"
 
-function SendData() {
-  console.log("Sending the dataE bro! -.-")
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  SendData = () => {
+    // GET request using fetch with async/await
+    const endPoint = 'localhost:4000/data';
+
+    //this.setState({ totalReactPackages: data.total })
+    console.log("Sending the dataE bro! -.-");
+  }
+
+  render() {
+    return (
+      <div style={{
+        padding: 20
+      }}>
+        <form onSubmit={this.SendData}>
+          <label >The data, bro: &nbsp; </label>
+          <input type="text" id="lname" name="lname" />
+          <br />
+          <button style={{
+            margin: 20
+          }} type="submit">
+            Send the data
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default function Index() {
-  return (
-    <div style={{
-      padding: 20
-    }}>
-      <form onSubmit={SendData}>
-        <label for="lname">The data, bro: &nbsp; </label>
-        <input type="text" id="lname" name="lname" />
-        <br />
-        <button style={{
-          margin: 20
-        }} type="submit">
-          Send the data
-        </button>
-      </form>
-    </div>
-  )
-}
+export default Index
