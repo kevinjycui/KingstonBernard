ba9df565b7204e20b112a3cd34d089fd841e3a5b
BluBloos <cnoah1705@gmail.com>
2021-01-23 21:25:51 -0500

    Added user interface

--- frontend/qhacks/src/pages/index.js
+++ frontend/qhacks/src/pages/index.js
@@ -1,7 +1,24 @@
 import React from "react"
 
+function SendData() {
+  console.log("Sending the dataE bro! -.-")
+}
+
 export default function Index() {
   return (
-    <p>Hello, </p>
+    <div style={{
+      padding: 20
+    }}>
+      <form onSubmit={SendData}>
+        <label for="lname">The data, bro: &nbsp; </label>
+        <input type="text" id="lname" name="lname" />
+        <br />
+        <button style={{
+          margin: 20
+        }} type="submit">
+          Send the data
+        </button>
+      </form>
+    </div>
   )
 }
